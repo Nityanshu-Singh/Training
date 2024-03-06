dd59de876c858141af5968a7a5b97d1305b9c24a
Nityanshu Singh <nityanshus@infinite.com>
2024-03-06 10:45:58 +0530

    Test 2

---
+++ Training/C Sharp/Assessment/Assessment2/Assessment2/Product.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment2
+{
+    class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+    }
+
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Product[] products = new Product[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Enter details for product {i + 1}: ");
+                Product product = new Product();
+
+                Console.WriteLine("Product ID: ");
+                product.ProductId = Convert.ToInt32(Console.ReadLine()); 
+                
+                Console.WriteLine("Product Name: ");
+                product.ProductName = Console.ReadLine();
+
+                Console.WriteLine("Price: ");
+                product.Price = Convert.ToDouble(Console.ReadLine());
+                products[i] = product;
+
+
+            }
+
+            Array.Sort(products, (x, y) => x.Price.CompareTo(y.Price));
+
+            Console.WriteLine("Sorted Products Based on Price: ");
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Product ID: {product.ProductId}, Product Name: {product.ProductName}, price: {product.Price} ");
+            }
+        }
+    }
+}
---
+++ Training/C Sharp/Assessment/Assessment2/Assessment2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment2
+{
+    abstract class Student
+    {
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public double Grade { get; set; }
+
+        public abstract bool IsPassed(double grade);
+    }
+
+    class Undergraduate : Student
+    {
+        public override bool IsPassed(double grade)
+        {
+            return grade > 70;
+        }
+    }
+
+    class Graduate : Student
+    {
+        public override bool IsPassed(double grade)
+        {
+
+            return grade > 80;
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter details for Undergraduate Student: ");
+            Undergraduate ug = new Undergraduate();
+            Console.WriteLine("Name: ");
+            ug.Name = Console.ReadLine();
+            Console.WriteLine("Student Id: ");
+            ug.StudentId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Grade: ");
+            ug.Grade = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter details for Graduate Student: ");
+            Graduate gr = new Graduate();
+            Console.WriteLine("Name: ");
+            gr.Name = Console.ReadLine();
+            Console.WriteLine("Student Id: ");
+            gr.StudentId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Grade: ");
+            gr.Grade = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Undergraduate Passed: " + ug.IsPassed(ug.Grade));
+            Console.WriteLine("Graduate Passed: " + gr.IsPassed(gr.Grade));
+
+            Console.ReadLine();
+        }
+        
+    }
+}
