a8f5f31732a8dfccc03daed4df76074c87dcdbb0
Nityanshu Singh <nityanshus@infinite.com>
2024-03-10 23:17:30 +0530

    Assign-4

---
+++ Training/C Sharp/Assigment/Assignment4/Concession/Concession/TicketConcession.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concession
+{
+    public class TicketConcession
+    {
+        public const double TotalFare = 500;
+
+        public static void CalculateConcession(int age, string name)
+        {
+            if (age <= 5)
+            {
+                Console.WriteLine($"Little Champs- Free Ticket for {name} ({age} years old) \n Ticket Status- Booked");
+            }
+            else if (age > 60)
+            {
+                double concessionAmount = TotalFare * 0.30;
+                double discountedFare = TotalFare - concessionAmount;
+                Console.WriteLine($"Senior Citizen - Discounted Fare: (discountedFare:C2} for {name} ({age} years old) \n Ticket Status- Booked");
+            }
+            else
+            {
+                Console.WriteLine($"Ticket Booked - Fare: {TotalFare:C2} for {name} ({age} years old) \n Ticket Status- Booked");
+            }
+        }
+    }
+}
---
+++ Training/C Sharp/Assigment/Assignment4/Concession/ConcessionClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Concession;
+
+namespace ConcessionClient
+{
+    class Program
+    {
+        static int TotalFare = 500;
+        static string Name;
+        static int Age;
+
+        public string _Name { get; set; }
+        
+
+        public int _Age { get; set; }
+        
+        static void Main(string[] args)
+        {
+            Program pro = new Program();
+            Console.WriteLine("Enter the Name of the Passenger:- ");
+            pro._Name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter the Age of the Passenger:- ");
+            pro._Age = Convert.ToInt32(Console.ReadLine());
+            TicketConcession tc = new TicketConcession();
+
+            TicketConcession.CalculateConcession(pro._Age, pro._Name);
+            Console.ReadLine();
+
+        }
+    }
+}
